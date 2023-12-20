22c7bc23b7e9a1411244864f3c9f66534182a1fc
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-20 01:23:13 +0100

    connected database

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,23 +1,57 @@
 const express = require("express");
-
 const router = express.Router();
+const Post = require("../models/Post");
 
-// Routes
-router.get("/", (req, res) =>{
-    const local = {
+/**
+ * GET /
+ * HOME
+ */
+router.get("/", async (req, res) => {
+ try {
+    const locals = {
         title: "NodeJs Blog",
-        description: "Simple Blog made with NodeJs, Express, EJS and MongoDB" 
-    }
+        description: "Simple Blog made with NodeJs, Express, EJS and MongoDB"
+      };
+    
+      let perPage = 10;
+      let page = req.query.page || 1;
 
-    res.render("index", local);
+      const data = await Post.aggregate([ { $sort: {createdAt: -1} } ])
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .exec();
+
+      const count = await Post.countDocuments({});
+      const nextPage = parseInt(page) + 1;
+      const hasNextPage = nextPage >= Math.ceil(count/ perPage);
+
+      console.log(nextPage);
+      console.log(hasNextPage);
+      console.log(req.query);
+
+    
+    res.render("index", { 
+        locals,
+        data,
+        current: page,
+        nextPage: hasNextPage ? nextPage : null
+        });
+} catch (error) {
+    console.log(error);
+}
+
+  
 });
 
+
+
 router.get("/about", (req, res) => {
-    const local = {
-        title: "NodeJs Blog | About",
-        description: "Learn about the Simple Blog made with NodeJs, Express, EJS and MongoDB" 
-    }
-    res.render("about", local)
-})
+  const local = {
+    title: "NodeJs Blog | About",
+    description:
+      "Learn about the Simple Blog made with NodeJs, Express, EJS and MongoDB"
+  };
+  res.render("about", local);
+});
 
 module.exports = router;
