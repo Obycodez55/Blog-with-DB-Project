df42d7b7700b9953744d0eae897780f02b1919ed
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-25 00:47:20 +0100

    error testing

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-
+const adminLayout = "../views/layouts/admin";
 /**
  * GET /
  * Admin -- Login
@@ -16,9 +19,7 @@ router.get("/admin", async (req, res) => {
         description: "Simple Blog created with NodeJs, Express & MongoDb."
       }
     
-
-    const data = await Post.find();
-    res.render('admin/index', { locals});
+    res.render('admin/index', { locals, layout: adminLayout});
   } catch (error) {
     console.log(error);
   }
@@ -26,8 +27,60 @@ router.get("/admin", async (req, res) => {
 });
 
 
+/**
+ * POST /
+ * Admin --Check Login
+ */
+
+router.post("/admin", async (req, res) => {
+  
+  try {
+    
+    const {username, password} = req.body;
 
 
+
+
+
+    res.redirect("/admin");
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+
+// /**
+//  * POST /
+//  * Admin --Register
+//  */
+
+// router.post("/register", async (req, res) => {
+  
+//   try {
+    
+//     const {username, password} = req.body;
+//     const hashedPassword = await bcrypt.hash(password, 10)
+    
+//     try {
+//       const user = await User.create({username, password:hashedPassword});
+//       res.status(201).json({message: "User created successfully", user})
+
+
+//     } catch (error) {
+//       if (error.code === 11000){
+//         res.status(409).json({message: "User Already in use"});
+//       }
+//       res.status(500).json({message: "Internal server error"});
+//     }
+
+
+   
+//   } catch (error) {
+//     console.log(error);
+//   }
+
+// });
 
 module.exports = router;
 
