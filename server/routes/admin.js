99600af8a3347709296624c8130c7ec3c329cc98
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-25 23:52:30 +0100

    Added the Edit and Delete routes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -4,33 +4,10 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const authMiddleware =require("../../middlewares/authMiddleware");
+const authMiddleware = require("../../middlewares/authMiddleware");
 
 const adminLayout = "../views/layouts/admin";
 const jwtSecret = process.env.JWT_SECRET;
-
-
-/**
- *
- * Guard Login
- */
-// const authMiddleware = (req, res, next) => {
-//   const token = req.cookies.token;
-
-//   if (!token) {
-//     return res.status(401).json( { message: "Unauthorized" } );
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, jwtSecret);
-//     req.userId = decoded.userId;
-//     next();
-//   } catch(error){
-//     res.status(401).json( { message: "Unauthorized" } );
-//   }
-// }
-
-
 
 /**
  * GET /
@@ -60,11 +37,11 @@ router.post("/admin", async (req, res) => {
     const { username, password } = req.body;
 
     const user = await User.findOne({ username });
-    
+
     if (!user) {
       return res.status(401).json({ message: "Invalid Credientials" });
     }
-    
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
@@ -85,27 +62,126 @@ router.post("/admin", async (req, res) => {
  * Admin Dashboard
  */
 
-router.get("/dashboard", authMiddleware , async (req, res) => {
-
-
+router.get("/dashboard", authMiddleware, async (req, res) => {
   try {
     const locals = {
       title: "Admin || Dashboard",
       description: "Simple Blog created with NodeJs, Express & MongoDb."
     };
 
+    const data = await Post.find();
+    res.render("admin/dashboard", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /
+ * Admin -- Create new post
+ */
+router.get("/add-post", authMiddleware, async (req, res) => {
+  try {
+    const locals = {
+      title: "Admin || Create!",
+      description: "Simple Blog created with NodeJs, Express & MongoDb."
+    };
 
     const data = await Post.find();
-    res.render("admin/dashboard", {locals, data});
+    res.render("admin/add-post", { locals, data, layout: adminLayout });
   } catch (error) {
-    
+    console.log(error);
   }
-
-
-
- 
-
 });
+
+/**
+ * POST /
+ * Admin -- Create new post
+ */
+
+router.post('/add-post', authMiddleware, async (req, res) => {
+  try {
+    try {
+      const newPost = new Post({
+        title: req.body.title,
+        body: req.body.body
+      });
+
+      await Post.create(newPost);
+      res.redirect('/dashboard');
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * GET /
+ * Admin -- Edit post
+ */
+router.get("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    const data = await Post.findById({ _id: slug });
+
+    const locals = {
+      title: "Admin Edit || "+ data.title,
+      description: "Simple Blog made with NodeJs, Express, EJS and MongoDB"
+    };
+
+    res.render("admin/edit-post", { locals, data, layout: adminLayout });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/**
+ * PUT /
+ * Admin -- Edit post
+ */
+
+router.put("/edit-post/:id", authMiddleware, async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    await Post.findByIdAndUpdate(slug, {
+      title: req.body.title,
+      body: req.body.body,
+      updatedAt: Date.now()
+    });
+
+  res.redirect(`/edit-post/${req.params.id}`);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+/**
+ * DELETE /
+ * Admin -- Delete post
+ */
+
+router.delete("/delete-post/:id", authMiddleware, async (req, res) => {
+  try {
+    let slug = req.params.id;
+
+    await Post.findByIdAndDelete(slug);
+
+  res.redirect("/dashboard");
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 
 /**
  * POST /
