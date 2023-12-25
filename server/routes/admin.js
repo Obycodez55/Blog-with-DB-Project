851cc2efac480ed693108c3835ec734cf03e710f
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-25 01:05:21 +0100

    started adding admin pahe

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../models/Post");
 
-
+const adminLayout = "../"
 /**
  * GET /
- * Admin -- Login
+ * Admin -- Login Page
  */
 
 router.get("/admin", async (req, res) => {
