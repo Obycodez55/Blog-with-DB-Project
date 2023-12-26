4460de63e141076369ff69fd71cfb188e4e84248
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2023-12-26 01:21:56 +0100

    Initialized tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./src/**/*.{html,js,ejs}",
+    "./views/**/*.{html,js,ejs}",
+    "./public/**/*.{html,js,ejs}",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
